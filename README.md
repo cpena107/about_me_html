354e41c2eb623fc038337cbcf38ceed2d9a5d0ea
Carlos Pena Caballero <andrescapc@aol.com>
2022-09-15 10:57:07 -0500

    Ready
    Update part2_tests.py
    
    Update part1_tests.py
    
    Update README.md
    
    Update part2_tests.py
    
    Update part1_tests.py
    
    Fixed!
    
    fixed typo in tests
    
    tests
    
    tests
    
    Update README.md
    
    README
    
    Add files via upload

---
+++ README.md
@@ -0,0 +1,40 @@
+# About me Part 1
+
+Make a web page that contains some information about yourself and what your career goals are. What do you want to do after you graduate? What are you interested in CS-wise? Why are taking this class?
+
+I'm also a little out of touch with everything. Please culture me. Can you recommend me some music, movies, and/or TV shows? 
+
+## HTML Requirements
+Your web page should include h1, p, img, ul, li, and a tags. As well as a table. 
+
+## CSS Requirements
+Your CSS should include tag, class, and id selectors. It should also include styling for fonts (color, size, decoration), backgrounds (color or image), and borders (to emphasize certain areas).
+
+You will not receive credit if you use inline-css. You must use a separate stylesheet. 
+
+Good HTML/CSS resource:  https://www.w3schools.com/
+
+
+# About me Part 2
+
+Make a web page that contains some information about yourself and what your career goals are. What do you want to do after you graduate? What are you interested in CS-wise? Why are taking this class?
+
+I'm also a little out of touch with everything. Please culture me. Can you recommend me some music, movies, and/or TV shows?
+
+## HTML Requirements
+
+Your web page should include the same content as part 1. Using Bootstrap could inspire you to do add more to make it look even better which is ok. (such as supplementing music recommendations with album art). Use Bootstrap to make everything look good. You might need to group elements in  to apply some styles.
+## CSS Requirements
+You do not need additional styling on top of Bootstrap. Just the bootstrap CSS from CDN. 
+
+Some Bootstrap resources:
+* https://getbootstrap.com/
+* https://getbootstrap.com/docs/4.0/layout/grid/
+* https://getbootstrap.com/docs/5.2/getting-started/introduction/
+
+<strong>For full credit, this version should look professional and utilize Bootstraps grid system (container, row, col) in at least one area.</strong>
+
+# Testing
+
+This assignment uses pytest and BeautifulSoup to test your HTML pages, only the HTML file is automatically tested, and the CSS portion needs to be manually checked.
+To run the tests use this command ```pytest tests/*``` or replace `*` with `part1_tests.py` or `part2_tests.py`
---
+++ tests/part2_tests.py
@@ -0,0 +1,122 @@
+from bs4 import BeautifulSoup
+import os
+import sys
+import re
+
+# container
+def test_container():
+    soup = BeautifulSoup(open('./about_me_part2.html'), "html.parser")
+    try:
+        cont = soup.select(".container")
+    except:
+        cont = None
+    assert cont is not None
+
+# row
+def test_row():
+    soup = BeautifulSoup(open('./about_me_part2.html'), "html.parser")
+    try:
+        cont = soup.select(".row")
+    except:
+        cont = None
+    assert cont is not None
+
+# container
+def test_column():
+    soup = BeautifulSoup(open('./about_me_part2.html'), "html.parser")
+    try:
+        cont = soup.select('[class*="col"]')
+    except:
+        cont = None
+    assert cont is not None
+
+# h1
+def test_header():
+    soup = BeautifulSoup(open('./about_me_part2.html'), "html.parser")
+    try:
+        header = soup.find("h1")
+    except:
+        header = None
+    assert header is not None
+
+# p
+def test_p():
+    soup = BeautifulSoup(open('./about_me_part2.html'), "html.parser")
+    try:
+        p = soup.find("p")
+    except:
+        p = None
+    assert p is not None
+
+# image
+def test_img():
+    soup = BeautifulSoup(open('./about_me_part2.html'), "html.parser")
+    try:
+        img = soup.find("img")
+    except:
+        img = None
+    assert img is not None
+    assert img.get('src') is not None
+
+# table
+def test_table():
+    soup = BeautifulSoup(open('./about_me_part2.html'), "html.parser")
+    try:
+        table = soup.find("table")
+    except:
+        table = None
+    assert table is not None
+
+# table headers
+def test_tableHeaders():
+    soup = BeautifulSoup(open('./about_me_part2.html'), "html.parser")
+    try:
+        th = soup.find("table").find_all("th")
+    except:
+        th = None
+    assert th is not None
+
+# table rows
+def test_tableRows():
+    soup = BeautifulSoup(open('./about_me_part2.html'), "html.parser")
+    try:
+        tr = soup.find("table").find_all("tr")
+    except:
+        tr = None
+    assert tr is not None
+
+# table data
+def test_tableData():
+    soup = BeautifulSoup(open('./about_me_part2.html'), "html.parser")
+    try:
+        td = soup.find("table").find_all("td")
+    except:
+        td = None
+    assert td is not None
+
+# unordered list
+def test_ul():
+    soup = BeautifulSoup(open('./about_me_part2.html'), "html.parser")
+    try:
+        ul = soup.find("ul")
+    except:
+        ul = None
+    assert ul is not None
+
+# list items x5
+def test_li():
+    soup = BeautifulSoup(open('./about_me_part2.html'), "html.parser")
+    try:
+        li = soup.find("ul").find_all("li")
+    except:
+        li = None
+    assert li is not None
+
+# link
+def test_link():
+    soup = BeautifulSoup(open('./about_me_part2.html'), "html.parser")
+    try:
+        a = soup.find("a")
+    except:
+        a = None
+    assert a is not None
